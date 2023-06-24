3c25f00af6412bc738b9125971cd60989d1f5e31
rajarathinam97 <rajarathinam057@gmail.com>
2023-06-24 13:23:36 +0530

    test

---
+++ src/org/college/College.java
@@ -0,0 +1,24 @@
+package org.college;
+
+public class College 
+{
+	public void collegeName() 
+	{
+		System.out.println("college name is ANNA UNIVERSITY");
+	}
+	public void collegeCode() 
+	{
+		System.out.println("college code is 346783");
+	}
+	public void collegeRank() 
+	{
+		System.out.println("college rank is SECOND RANK");
+	}
+public static void main (String[] args) 
+{
+	College g=new College();
+	g.collegeName();
+	g.collegeCode();
+	g.collegeRank();
+}
+}
